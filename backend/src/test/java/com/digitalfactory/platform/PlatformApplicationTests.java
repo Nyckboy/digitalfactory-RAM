b39d80393872bfafe8dc51d70d60dedbfd633daa
nyckboy <abasside132@gmail.com>
2026-07-02 11:00:17 +0100

    feat(front/back): initializing the project

---
+++ backend/src/test/java/com/digitalfactory/platform/PlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalfactory.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
