a16c383037eba43938068a492f58838af05d46b1
nyckboy <abasside132@gmail.com>
2026-08-08 15:02:37 +0100

    fix: configure a fake url test  to pass the test build

--- backend/src/test/java/com/digitalfactory/platform/PlatformApplicationTests.java
+++ backend/src/test/java/com/digitalfactory/platform/PlatformApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(properties = {
     // These are literally just fake strings I typed out. Spring will accept them!
     "application.security.jwt.secret-key=this-is-just-a-fake-key-to-make-the-test-happy-12345",
-    "ai.api.key=fake-ai-key"
+    "ai.api.key=fake-ai-key",
+		"ai.api.url=http://this-is-a-fake-url.com"
 })
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class PlatformApplicationTests {
